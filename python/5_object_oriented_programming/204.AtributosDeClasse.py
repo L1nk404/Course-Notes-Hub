410e768ea575a683d101a6958c5954b8361829b4
Link <gabrielhmorais98@gmail.com>
2023-11-23 17:33:16 -0300

    Python Lessons

---
+++ python/5_object_oriented_programming/204.AtributosDeClasse.py
@@ -0,0 +1,26 @@
+
+# atributos de classe são atributos criados no escopo da classe
+
+
+class Pessoa:
+    ano_atual = 2022  # Atributo de classe
+
+    def __init__(self, nome, idade):
+        self.nome = nome
+        self.idade = idade
+        """ 
+        Se mudarmos aqui o valor ano atual atrapalharemos a conta, pois
+        ele busca primeiro o valor na instância e depois na classe.
+        """
+        # self.ano_atual = 100
+
+    def get_ano_nascimento(self):
+        return Pessoa.ano_atual - self.idade  # Usando o atributo de classe
+
+
+p1 = Pessoa('João', 35)
+p2 = Pessoa('Helena', 12)
+print(Pessoa.ano_atual)  # Podemos chamar o atributo fora da classe
+# Pessoa.ano_atual = 1 # Note que também podemos mudar o valor do mesmo
+print(p1.get_ano_nascimento())
+print(p2.get_ano_nascimento())
---
+++ python/5_object_oriented_programming/exercicios/206.salvandoJSON.py
@@ -0,0 +1,8 @@
+""" 
+Exerício - Salve sua classe em JSON.
+
+Salve os dados da sua classe sm Json e depois crie novamente as instâncias
+da classe com os dados salvos.
+
+Faça em programas separados.
+"""
